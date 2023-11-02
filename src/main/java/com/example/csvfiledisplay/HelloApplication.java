6c4871d2a1842d737bb1f5147379147b4cedce8e
leonlolleonlol <leonkoj2004@gmail.com>
2023-11-02 16:14:06 -0400

    remove unecessary columns

--- src/main/java/com/example/csvfiledisplay/HelloApplication.java
+++ src/main/java/com/example/csvfiledisplay/HelloApplication.java
@@ -4,42 +4,21 @@ package com.example.csvfiledisplay;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
 public class HelloApplication extends Application {
-    public class Record {
-
-        private Map<String, SimpleStringProperty> fields;
-        public SimpleStringProperty getFieldProperty(String fieldName) {
-            return fields.get(fieldName);
-        }
-
-        public Record(String[] fieldNames, String[] fieldValues) {
-            if (fieldNames.length != fieldValues.length) {
-                throw new IllegalArgumentException("Field names and values arrays must have the same length.");
-            }
-
-            this.fields = new HashMap<>();
-            for (int i = 0; i < fieldValues.length; i++) {
-                this.fields.put(fieldNames[i], new SimpleStringProperty(fieldValues[i]));
-            }
-        }
-
-
-    }
     private int[] maxes;
     private final TableView<Record> tableView = new TableView<>();
 
@@ -49,27 +28,35 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("Display");
-        readCSV("WASTE_BIN_TYPE.csv");
+        primaryStage.setHeight(Screen.getPrimary().getVisualBounds().getHeight()*0.9);
+        primaryStage.setWidth(Screen.getPrimary().getVisualBounds().getWidth()*0.9);
+        readCSV("WASTE_INVOICES.csv");
         Group root = new Group();
         TableColumn<Record, String>[] columns = new TableColumn[numberOfColumns];
         for (int i = 0; i < numberOfColumns; i++) {
             int index = i + 1;
             columns[i] = new TableColumn<>("f" + index);
             columns[i].setCellValueFactory(data -> data.getValue().getFieldProperty("f" + index));
-            columns[i].setStyle("-fx-font-size: 10;");
-            columns[i].setPrefWidth(Math.min(maxes[i]*5.66,1650/numberOfColumns));
+            columns[i].setStyle("-fx-font-size: 12;");
         }
+        tableView.getColumns().stream().forEach( (column) -> column.setPrefWidth( maxes[column.getText().charAt(1)] + 10.0d ));
         tableView.setItems(dataList);
         for (var i: columns)
             tableView.getColumns().add(i);
-
         VBox vBox = new VBox();
-
+        vBox.setAlignment(Pos.CENTER);
+        //primaryStage.sizeToScene();
+        //tableView.setColumnResizePolicy( TableView.UNCONSTRAINED_RESIZE_POLICY);
+        int desiredWidth=0;
+        for(int i:maxes)
+            desiredWidth+=i;
+        desiredWidth*=5.66;
+        tableView.setMinSize( desiredWidth-25, primaryStage.getHeight());
         vBox.getChildren().add(tableView);
-
+        vBox.setMinWidth(desiredWidth*0.9);
+        //vBox.setFillWidth(true);
         root.getChildren().add(vBox);
-
-        primaryStage.setScene(new Scene(root, 1000, 750));
+        primaryStage.setScene(new Scene(root, desiredWidth, primaryStage.getHeight()));
         primaryStage.show();
 
     }
@@ -84,12 +71,29 @@ public class HelloApplication extends Application {
         String line;
         while ((line = br.readLine()) != null) {
             String[] fields = line.split(FieldDelimiter, numberOfColumns);
+            String[] realFields = new String[numberOfColumns];
             String[] names = new String[numberOfColumns];
+            int indexCommaDelimiterInMoneyString=-1;
             for (int n = 0; n < numberOfColumns; n++) {
                 names[n] = "f" + (n + 1);
                 maxes[n]=Math.max(fields[n].length(),maxes[n]);
+                if (fields[n].contains("$")&&fields[n+1].matches("^[0-9]+(\\.[0-9]+)?(\"?)+$")) {
+                    realFields[n]=fields[n]+" "+fields[n+1];
+                    indexCommaDelimiterInMoneyString=n;
+                }
+                if(n!=numberOfColumns-1)
+                {
+                if(indexCommaDelimiterInMoneyString<n)
+                    realFields[n]=fields[n+1].isEmpty()?"-":fields[n+1];
+                else if (indexCommaDelimiterInMoneyString!=n)
+                    realFields[n]=fields[n].isEmpty()?"-":fields[n];
+                }
+                else if(indexCommaDelimiterInMoneyString<n)
+                    realFields[n]="-";
+
+
             }
-            HelloApplication.Record record = new HelloApplication.Record(names, fields);
+            Record record = new Record(names, realFields);
             dataList.add(record);
         }
     }
