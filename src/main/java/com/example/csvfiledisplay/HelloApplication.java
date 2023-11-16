3001d200bd12cac960bbdbf20c12bbe6536a6b07
leonlolleonlol <leonkoj2004@gmail.com>
2023-11-16 09:53:05 -0500

    Revert "bug fixes+ better parsing+ include all csvs 11/15"
    This reverts commit 478f4d8ac14482538e8e7fc37b68f5e7fc4f48fa.

--- src/main/java/com/example/csvfiledisplay/HelloApplication.java
+++ src/main/java/com/example/csvfiledisplay/HelloApplication.java
@@ -2,7 +2,6 @@ package com.example.csvfiledisplay;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -35,7 +34,7 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
-    private static String previousChoice = "CU_SR_OPEN_DATA_CATALOG.csv", forcedDelimiter = null;
+    private static String previousChoice = "CATALOG_2023_09_19.csv", forcedDelimiter = null;
     public static final double RATIO_CONTENT_TO_WINDOW = 0.72;
     private static Hyperlink hyperlink = new Hyperlink("www.github.com/leonlolleonlol");
     private static boolean actualFileImported = false, finished, iJustPressedAkey = false, lineChange;
@@ -49,31 +48,10 @@ public class HelloApplication extends Application {
     public void start(Stage primaryStage) throws IOException {
         changingStage = primaryStage;
         changingStage.setMaximized(true);
-        cb.getItems().addAll(
-        "BUILDING_LIST.csv",
-        "CATALOG_2022_08_16.csv",
-        "CATALOG_2022_08_18.csv",
-        "CATALOG_2022_12_21.csv",
-        "CATALOG_2023_03_30.csv",
-        "CATALOG_2023_09_18.csv",
-        "CATALOG_2023_09_19.csv",
-        "CATALOG.csv",
-        "CU_SR_OPEN_DATA_CATALOG_DESC.csv",
-        "CU_SR_OPEN_DATA_CATALOG.csv",
-        "CU_SR_OPEN_DATA_COMB_SECTIONS.csv",
-        "CU_SR_OPEN_DATA_DEPT_FAC_STRUC.csv",
-        "CU_SR_OPEN_DATA_SCHED.csv",
-        "CU_SR_OPEN_DATA_TERM_SESS.csv",
-        "DATA_OUTPUT.csv",
-        "DATAOUTPUT_MODIFIED.csv",
-        "POINT_LIST.csv",
-        "WASTE_BIN_TYPE.csv",
-        "WASTE_INVOICES.csv",
-        "WASTE_TYPE.csv");
-        int random=new Random().nextInt(cb.getItems().size()-1);
-        if(random==12)
-            random++;
-        previousChoice=cb.getItems().get(random);
+        cb.getItems().addAll("DATAOUTPUT_MODIFIED.csv", "WASTE_BIN_TYPE.csv", "WASTE_INVOICES.csv",
+                "CATALOG_2022_08_16.csv", "CATALOG_2023_09_19.csv", "CATALOG.csv",
+                "CU_SR_OPEN_DATA_TERM_SESS.csv",
+                "DATA_OUTPUT.csv", "FacList.csv", "POINT_LIST.csv", "BUILDING_LIST.csv", "WASTE_TYPE.csv");
         changingStage.getIcons().add(new Image("file:download.png"));
 
         cb.setValue(previousChoice);
@@ -107,7 +85,9 @@ public class HelloApplication extends Application {
                 TaskDisplayTable taskDisplayTable = new TaskDisplayTable(new Group(), startTime);
                 Thread thread1 = new Thread(taskDisplayTable);
                 cb.setStyle(
-                        "-fx-font-family: Arial; -fx-font-size: 12px;");
+                        "-fx-font-family: Arial; -fx-font-size: "
+                                + (25 * Math.pow(Math.E, -0.1 * cb.getItems().size()) + 4)
+                                + "px;");
                 thread1.start();
             });
         } catch (Exception e) {
